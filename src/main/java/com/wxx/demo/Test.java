29c5f707fbbfb49fd818584e1ea2e21279a45aad
wangxinxin07 <wangxinxin07@58.com>
2019-07-13 15:12:26 +0800

    初始化

---
+++ src/main/java/com/wxx/demo/Test.java
@@ -0,0 +1,15 @@
+package com.wxx.demo;
+
+/**
+ * Test.java
+ *
+ * @author wangxinxin07
+ * @date 2019/7/13
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
